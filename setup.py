882719fe90cb6045115a00b72a978053a1bae643
sbien1208 <148199196+sbien1208@users.noreply.github.com>
2024-01-30 09:08:23 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='Todo-list-application_Samantha_Biennassis_CDOF2',
+ version='1.0',
+ author='sbien1208',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
